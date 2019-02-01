48ffa00bea4391f56d11e7b07317e2ea9a15a3a7
Vedant Jagani <ved1007@gmail.com>
2019-01-31 22:13:36 -0600

    1st Commit from Vedant

---
+++ JavaTraining/src/JavaBasic/IfElseConcept.java
@@ -0,0 +1,48 @@
+package JavaBasic;
+
+public class IfElseConcept {
+
+	public static void main(String[] args) {
+		
+		
+		int a = 100;
+		int b = 200;
+		
+		if(b>a){
+			System.out.println("B is greater than A");
+		}
+		else {
+			System.out.println("a is greater than b");
+		}
+		
+		// comparison operators
+		// < > <= >= == !=
+		// == is comparison operators
+		
+		int c = 60;
+		int d = 60;
+		
+		if(c==d){
+			System.out.println("c and d are equal");
+			
+		}
+		else{
+			
+			System.out.println("c and d are not equal");
+		}
+		
+		// How to find highest #s
+		
+		int a1 = 100;
+		int b1 = 200;
+		int c1 = 300;
+		
+		if(a1>b1 & a1<c1){
+			System.out.println("a1 is the geater");
+		}
+		else {
+			System.out.println("b1 is geate than a1");
+		}
+	}
+
+}
---
+++ JavaTraining/src/JavaBasic/StringConcatenation.java
@@ -0,0 +1,34 @@
+package JavaBasic;
+
+public class StringConcatenation {
+
+	public static void main(String[] args) {
+		
+		
+		// plus sign is concatenation operator 
+		// println -- Is used to print on the console with a new line
+		// print is just used to print on the console 
+		int a = 100;
+		int b = 200;
+		
+		String x = "Hello";
+		String y = "World";
+		
+		double c = 10.00;
+		double d = 15.00;
+		
+		System.out.println(a+b);
+		System.out.println(x+y);
+		
+		System.out.println(a+b+x);
+		
+		System.out.println(c+d);
+		System.out.println("value of a and b is =" +a+b);
+		System.out.println("value of A is = "+a);
+		
+		System.out.print("Hello Selenium");
+		System.out.println("Testers");
+
+	}
+
+}
